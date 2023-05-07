a7a9396522e6377ac2f9d2b99693743d76451f73
pmarangon <56054345+pmarangon@users.noreply.github.com>
2023-05-07 15:28:59 -0300

    Adiciona Url e view

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from tarefas.models import Tasks
+import datetime#
 
-# Create your views here.
+
+def tasks_view(request):
+ tasks =  Tasks.objects.all
+ print(tasks)
+  return render(
+ request, 'tasks.html',
+{'tarefas':{'tarefa':'ToDo'}}
+        
+)
+
+
