319c50d97a546fa4594f8bac2b08ae90476cd61b
pmarangon <56054345+pmarangon@users.noreply.github.com>
2023-05-07 15:34:30 -0300

    adiciona teste unitário

--- tarefas/tests.py
+++ tarefas/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
-# Create your tests here.
+class TasksTest(TestCase):
+ def setUp(self):
+ self.resp = self.client.get('/')
+ def test_200_response(self):
+ self.assertEqual(200, self.resp.status_code)
+ def test_texto(self):
+ self.assertContains(self.resp, 'tarefa')
